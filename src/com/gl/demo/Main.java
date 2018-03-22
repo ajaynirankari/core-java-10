576b61645ce0bd52e2ad6e4080c9407dc73e523f
Ajay.Nirankari <ajaykumarnirankarigl@gmail.com>
2018-03-22 15:03:17 +0530

    Initial commit

---
+++ src/com/gl/demo/Main.java
@@ -0,0 +1,14 @@
+package com.gl.demo;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var list = List.of(1,2,3,4,5);
+
+        System.out.println(list);
+
+    }
+}
